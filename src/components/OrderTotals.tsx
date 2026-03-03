9e9864f14787e0cd81abab2a28bfdcffe64a8098
HugoBzn <hugobazan1499@gmail.com>
2026-03-03 12:33:26 -0600

    Finalizacion

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -5,9 +5,10 @@ import { formatCurrency } from "../helpers";
 type OrderTotalsProps = {
   order: OrderItem[];
   tip: number;
+  placeOrder: () => void;
 };
 
-export default function OrderTotals({ order, tip }: OrderTotalsProps) {
+export default function OrderTotals({ order, tip, placeOrder }: OrderTotalsProps) {
   const subtotalAmount = useMemo(() => order.reduce((total, item) => total + item.quantity * item.price, 0), [order]);
   const tipAmount = useMemo(() => subtotalAmount * tip, [tip, subtotalAmount]);
   const totalAmount = useMemo(() => subtotalAmount + tipAmount, [subtotalAmount, tipAmount]);
@@ -29,7 +30,13 @@ export default function OrderTotals({ order, tip }: OrderTotalsProps) {
         </p>
       </div>
 
-      <button></button>
+      <button
+        className="w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10 enabled:cursor-pointer"
+        disabled={totalAmount === 0}
+        onClick={placeOrder}
+      >
+        Guardar Orden
+      </button>
     </>
   );
 }
